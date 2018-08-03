9b09b7ccf792c9d9ebb8102becbc5af741fe9522
Anita Beauchamp <amadamspr@gmail.com>
2018-08-03 21:07:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
 
     if self.len_1 <= 0 || self.len_2 <= 0 || self.len_3 <= 0
       raise TriangleError
-    elsif len_1_2 < self.len_3 || len_1_3 < self.len_2 || len_2_3 < self.len_1
+    elsif len_1_2 <= self.len_3 || len_1_3 <= self.len_2 || len_2_3 <= self.len_1
       raise TriangleError
     elsif self.len_1 == self.len_2 && self.len_1 == self.len_3
       :equilateral

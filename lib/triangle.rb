832d202af797f2e41cbceb9ad9b563e730cfeb3c
Anita Beauchamp <amadamspr@gmail.com>
2018-08-03 20:56:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,4 +2,12 @@ class Triangle
   attr_accessor :len_1, :len_2, :len_3
 
   def initialize(len_1, len_2, len_3)
+    self.len_1 = len_1
+    self.len_2 = len_2
+    self.len_3 = len_3
+  end
+
+  def kind
+
+  end
 end
